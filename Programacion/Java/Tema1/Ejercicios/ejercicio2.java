31abd102111362920fc617a1f8381ec097b5f9ee
Lucia <lucilu.daw@gmail.com>
2024-10-20 20:54:22 +0200

    Programacion: ejercicios relacion 1

---
+++ Programacion/Java/Tema1/Ejercicios/ejercicio2.java
@@ -0,0 +1,12 @@
+public class ejercicio2 {
+    public static void main(String[] args) {
+        
+        String nombre = "Me llamo Lucia";
+        String direccion = "Calle Limonar";
+        int num = 237234823;
+
+        System.out.println(nombre);
+        System.out.println("Vivo en " + direccion);
+        System.out.println("Mi numero de telefono es: "+ num);
+    }
+}
