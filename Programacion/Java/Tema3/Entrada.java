ac49c0d54481715a3fdae8e555cc0aaf3a0e995f
Lucia Villena Martin <lucilu.daw@gmail.com>
2024-10-23 13:59:50 +0200

    entrada java

---
+++ Programacion/Java/Tema3/Entrada.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Entrada {
+    public static void main(String[] args) {
+         
+        String nombre ;
+        int edad ;
+        String linea ;
+        float valor ;
+
+        /* 
+        //Solicitamos una cadena de caracteres:
+        System.out.println("¿Cómo te llamas?") ;
+        nombre = System.console().readLine() ; //Equivalente a Leer. 
+        //readLine SIEMPRE devuelve un string
+
+        //Solicitamos un valor de tipo entero:
+        System.out.println("¿Cuántos años tienes?");
+        linea = System.console().readLine() ;
+
+        //Convertimos a tipo ENTERO el valor guardado en la cadena:
+        edad = Integer.parseInt(linea) ;
+
+        //Solicitamos un valor de tipo real:
+        System.out.println("Dime un valor de tipo real: ") ;
+        linea = System.console().readLine() ;
+
+        //Convertimos a tipo real el valor guardado en la cadena:
+        valor = Float.parseFloat(linea) ;        
+
+        //Mostramos un mensaje
+        System.out.println("Hola " + nombre + ". Encantada de conocerte.") ;
+        System.out.printf("Veo que tienes %d años.\n",edad) ;
+        System.out.printf("Tu valor de tipo real es %.2f.\n", valor) ; 
+        */
+
+        //UTILIZANDO LA CLASE SCANNER---------------------------------------
+
+       Scanner sc = new Scanner(System.in) ;
+
+        System.out.println("Introduce tu edad: ");
+        edad = sc.nextInt() ; //Lo leo directamente como entero
+
+        System.out.printf("Veo que tienes %d. y en 2025 tendrás %d",edad,++edad);
+    }
+}
