556c27c0810e8637ea48a99482378ed5c0511d5e
Lucia Villena Martin <lucilu.daw@gmail.com>
2024-10-28 14:39:14 +0100

    Clase 28/10/2024 prog

---
+++ Programacion/Java/Tema4/Apuntes/apuntes.java
@@ -0,0 +1,45 @@
+package Java.Tema4;
+
+import java.lang.runtime.SwitchBootstraps;
+
+public class apuntes {
+
+->Sentecia de seleccion
+
+    if (.."condicion"..) { 
+        instruccion a ejecutar si la condicion es verdadera 
+    } else {
+        instruccion a ejecutar si la condicion es falsa
+    }
+    
+->Sentecia de seleccion
+
+    if (.."condicion"..) { 
+        instruccion a ejecutar si la condicion es verdadera 
+    } else if {
+        instruccion a ejecutar si la condicion es verdadera
+    }
+
+
+-> Operador ternario //simplificar las sentencias de seleccion 
+
+    "aqui puedes poner cualquier cosa, desde una variable hasta algo mas "  = (.."condicion"..)?.."expresion1".. : .."expresion2".. ; //   ? : hace de entonces y : hace de else 
+                                                                                                    true               false
+    
+                                                                                                    
+    
+                                                                                                
+-> Switch
+
+    switch (.."expresion..")
+        case 1 : ......... 
+            break;     //el break es un salto INCONDICIONAL Y SOLO se usa con los switch
+        case 2 : ......... 
+            break;
+        case 3 : ......... 
+            break;
+        default //(opcional) 
+        
+    
+
+}
