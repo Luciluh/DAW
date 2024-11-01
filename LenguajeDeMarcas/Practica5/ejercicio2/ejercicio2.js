3e476ce540c9db69443b1a48997a9e06572cfc1c
Lucia <lucilu.daw@gmail.com>
2024-11-02 00:02:21 +0100

    EJERCICIOS TERMINADOS- LENGUAJE DE MARCAS

--- LenguajeDeMarcas/Practica5/ejercicio2/ejercicio2.js
+++ LenguajeDeMarcas/Practica5/ejercicio2/ejercicio2.js
@@ -1,9 +1,21 @@
-function CambioDimension(){
-    document.getElementById("dimension").style.weight = '500px'; 
-    document.getElementById("dimension").style.height = '500px'; 
+function CambioDimension() {
+    let cambioDim = document.getElementById("dimension");
 
+    if (cambioDim.style.width === '300px' && cambioDim.style.height === '300px') {
+        cambioDim.style.width = '700px';
+        cambioDim.style.height = '700px';
+    } else {
+        cambioDim.style.width = '300px';
+        cambioDim.style.height = '300px';
+    }
 }
 
-function CambioFoto(){
-    document.getElementById("dimension").src='recursos/aguacate_enamorado.avif' ;
+function CambioFoto() {
+    let cambioFot = document.getElementById("dimension");
+
+    if (cambioFot.src.includes('aguacate_amigos.avif')) {
+        cambioFot.src = 'recursos/aguacate_enamorado.avif';
+    } else {
+        cambioFot.src = 'recursos/aguacate_amigos.avif';
+    }
 }
