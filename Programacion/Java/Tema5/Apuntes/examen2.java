34b8db6164b2d8823939894b8042dbef1d67a2a9
Lucia <lucilu.daw@gmail.com>
2024-11-22 17:07:44 +0100

    .

---
+++ Programacion/Java/Tema5/Apuntes/examen2.java
@@ -0,0 +1,7 @@
+try {
+    // Código que puede lanzar una excepción
+} catch (IOException e) {
+    System.out.println("Se produjo un error de IO");
+} finally {
+    System.out.println("Este bloque siempre se ejecutará");
+}
